a4048ee4e94092cbaccde6af0f4927af2fdbe43b
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-07-29 05:08:38 +0000

    Added Kafka-Python Stuff

--- Kafka-Python-Events/producer.py
+++ Kafka-Python-Events/producer.py
@@ -1,49 +1,21 @@
-#!/usr/bin/env python
 
-import sys
-from random import choice
-from argparse import ArgumentParser, FileType
-from configparser import ConfigParser
-from confluent_kafka import Producer
+import json
+import time
 
-if __name__ == '__main__':
-    # Parse the command line.
-    parser = ArgumentParser()
-    parser.add_argument('config_file', type=FileType('r'))
-    args = parser.parse_args()
+from kafka import KafkaProducer
 
-    # Parse the configuration.
-    # See https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md
-    config_parser = ConfigParser()
-    config_parser.read_file(args.config_file)
-    config = dict(config_parser['default'])
+ORDER_KAFKA_TOPIC = "order_detail"
+ORDER_LIMIT = 15
 
-    # Create Producer instance
-    producer = Producer(config)
+producer = KafkaProducer(bootstrap_servers="localhost:9092")
 
-    # Optional per-message delivery callback (triggered by poll() or flush())
-    # when a message has been successfully delivered or permanently
-    # failed delivery (after retries).
-    def delivery_callback(err, msg):
-        if err:
-            print('ERROR: Message failed delivery: {}'.format(err))
-        else:
-            print("Produced event to topic {topic}: key = {key:12} value = {value:12}".format(
-                topic=msg.topic(), key=msg.key().decode('utf-8'), value=msg.value().decode('utf-8')))
+for i in range(ORDER_LIMIT):
+    data = {
+        "order_id": i,
+        "user_id": f"tom_{i}",
+        "total_cost": i,
+        "items": "burger,sandwich",
+    }
 
-    # Produce data by selecting random values from these lists.
-    topic = "purchases"
-    user_ids = ['eabara', 'jsmith', 'sgarcia', 'jbernard', 'htanaka', 'awalther']
-    products = ['book', 'alarm clock', 't-shirts', 'gift card', 'batteries']
-
-    count = 0
-    for _ in range(10):
-
-        user_id = choice(user_ids)
-        product = choice(products)
-        producer.produce(topic, product, user_id, callback=delivery_callback)
-        count += 1
-
-    # Block until the messages are sent.
-    producer.poll(10000)
-    producer.flush()
+    producer.send("order_details", json.dumps(data).encode("utf-8"))
+    print(f"Done Sending..{i}")
